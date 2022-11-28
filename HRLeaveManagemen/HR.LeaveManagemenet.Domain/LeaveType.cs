515fa09ba14e4388145076866f43c9faa71f35cc
Vlad Chirila <vlad.chirila@live.com>
2022-11-28 18:22:54 +0200

    remove cached files

--- HRLeaveManagemen/HR.LeaveManagemenet.Domain/LeaveType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HR.LeaveManagemenet.Domain
-{
-    using HR.LeaveManagemenet.Domain.Common;
-
-    public class LeaveType : BaseDomainEntity
-    {
-        public int DefaultDays { get; set; }
-    }
-}
--- HRLeaveManagemen/HR.LeaveManagement.Application/Persistance/Contracts/ILeaveRequestRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HR.LeaveManagement.Application.Persistance.Contracts
-{
-    using HR.LeaveManagemenet.Domain;
-
-    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
-    {
-
-    }
-}
