129b9ec0901934930355598014667ddf422484b3
Vlad Chirila <vlad.chirila@live.com>
2022-11-28 17:22:16 +0200

    Add LeaveType and LeaveRequest classes

---
+++ HRLeaveManagemen/HR.LeaveManagemenet.Domain/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace HR.LeaveManagemenet.Domain
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string ReuestComments { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approbed { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
---
+++ HRLeaveManagemen/HR.LeaveManagemenet.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.LeaveManagemenet.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
