c7761314fa4ae5bd8be86227bd6013a42212b934
Vlad Chirila <vlad.chirila@live.com>
2022-11-30 13:08:13 +0200

    Add first request and handler

---
+++ HRLeaveManagement/HR.LeaveManagement.Application/Features/LeaveTypes/Requests/GetLeaveTypeListRequest.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDto>>
+    {
+
+    }
+}
