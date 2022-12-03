7940c487519abe7cc7e4dbaa5a00e1234d651d92
Vlad Chirila <vlad.chirila@live.com>
2022-12-03 17:05:26 +0200

    Add all the forgoten mappings

--- HRLeaveManagement/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HRLeaveManagement/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -9,10 +9,24 @@
     {
         public MappingProfile()
         {
+            #region LeaveRequest Mappings
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion
+
+            #region LeaveAllocation Mappings
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
+
+            #region LeaveType Mappings
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            #endregion
+
         }
     }
 }
